44f4a87d1d35acc616712e5cf537548199647ed5
TanjibQuad <tanjib@quadque.tech>
2023-08-30 13:01:17 +0600

    pay slip status approve or disapprove

--- app/Http/Controllers/AccountantController.php
+++ app/Http/Controllers/AccountantController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Accountant;
 use App\Models\Student;
+use App\Models\Accountant;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class AccountantController extends Controller
 {
@@ -35,6 +36,42 @@ class AccountantController extends Controller
                 } else {
                     return response()->json([
                         'message'    => 'failed',
+                        'status' => 500
+                    ], 500);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'Unauthenticated',
+                    'status' => 401
+                ], 401);
+            }
+        } else {
+            return response()->json([
+                'message' => 'Unauthenticated',
+                'status' => 401
+            ], 401);
+        }
+    }
+
+    /////////////////Approve or Disapprove pay slip////////////////////
+    public function pay_slip_status_change(Request $request)
+    {
+        if ($request->bearerToken()) {
+            $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
+            $flag_receive = $flag['data'];
+            if ($flag_receive == 1) {
+                $student = Student::find($request->student_id);
+                $student->pay_slip_status = $request->status;
+                $update = $student->save();
+                if ($update) {
+                    return response()->json([
+                        'message'    => 'Status changed',
+                        'status' => 201,
+                        'data' => $update
+                    ], 201);
+                } else {
+                    return response()->json([
+                        'message'    => 'Failed',
                         'status' => 500
                     ], 500);
                 }
--- routes/accountant.php
+++ routes/accountant.php
@@ -9,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/file-name-and-variable', [AccountantController::class, 'course_file_upload']);
 Route::get('/get-payment-slip-lists', [AccountantController::class, 'payment_slip_lists']);
+Route::post('/pay-slip-status-change', [AccountantController::class, 'pay_slip_status_change']);
